a529fb30c565139656375692ed096c95d818b447
navidsotoudeh <navidsotoudehmehr@gmail.com>
2023-05-02 14:58:19 +0330

    redirect bug fixed 2

--- src/middlewares/withAuthentication.ts
+++ src/middlewares/withAuthentication.ts
@@ -2,11 +2,21 @@ import type { NextRequest } from 'next/server'
 import { verifyAuth } from '@/lib/auth'
 import { NextFetchEvent, NextResponse } from 'next/server'
 import { MiddlewareFactory } from '@/middlewares/types'
+import { cookies } from 'next/headers'
+// import jwt from 'jsonwebtoken'
+
+// function isTokenExpired(token: string) {
+//   const decoded = jwt.decode(token)
+//   const now = Date.now() / 1000
+//   return decoded && decoded.exp && decoded.exp < now
+// }
 
 export const withAuthentication: MiddlewareFactory = (next) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
-    if (request.nextUrl.pathname.includes('/users')) {
-      return NextResponse.redirect(new URL('/notes', request.url))
+    const token = request.cookies.get('notesapp-accessToken')?.value ?? ''
+
+    if (request.url.includes('/users') && !token) {
+      return NextResponse.redirect(new URL('/login', request.url))
     }
   }
 }
